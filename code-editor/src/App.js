c4f286168cee93e27afa43712ae6d6b9d8a4b70c
Nibalab <abdalbakinibal@gmail.com>
2024-07-25 02:21:53 +0300

    setting the page folder

--- code-editor/src/App.js
+++ code-editor/src/App.js
@@ -1,20 +1,31 @@
-// src/App.js
 import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
-import Navbar from '../src/components/Navbar/Navbar';
-import HeroSection from '../src/components/HeroSection/HeroSection';
-import LearnMoreSection from '../src/components/LearnMoreSection/LEarnMoreSection';
-import Footer from '../src/components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import LandingPage from '../src/pages/landingPage/LandingPage';
+// import RegistrationPage from '../src/pages/RegistrationPage/RegistrationPage';
+// import ProfilePage from '../src/pages/ProfilePage/ProfilePage';
+// import ProjectsPage from '../src/pages/ProjectPage/ProjectPage';
+// import SearchPage from '../src/pages/SearchPage/SearchPage';
+// import ChatPage from '../src/pages/ChatPage/ChatPage';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <HeroSection />
-      <LearnMoreSection />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+         */}
+      </Routes>
       <Footer />
     </div>
   );
 };
+
 export default App;
---
+++ code-editor/src/pages/landingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../../components/HeroSection/HeroSection';
+import LearnMoreSection from '../../components/LearnMoreSection/LEarnMoreSection';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <LearnMoreSection />
+    </div>
+  );
+};
+
+export default LandingPage;
