b4a3e59c70eaaf9bb0457426abbebd5602902f21
SelimChehwane <selim_chehwane@hotmail.com>
2024-07-27 18:09:28 +0300

    Adding search funtionality

--- code-editor/src/pages/user-browser-page.jsx
+++ code-editor/src/pages/user-browser-page.jsx
@@ -1,28 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ProfileCard from "../components/Profile-Card/profile_card";
-import axios from "axios";
+import SearchBar from "../components/SearchBar";
 
 const UserSearch = () => {
     const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/api/search'); 
-                setUsers(response.data.users);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        fetchUsers();
-    }, []);
+    const handleSearch = (user) => {
+        setUsers(user ? [user] : []); 
+    };
 
     return (
         <div className="window">
+            <SearchBar onSearch={handleSearch} /> 
             {users.length > 0 ? (
                 users.map(user => (
-                    <ProfileCard name={user.name} bio={user.email} />
+                    <ProfileCard key={user.id} name={user.name} bio={user.email} />
                 ))
             ) : (
                 <p>No users found.</p>
