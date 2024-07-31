f8716444f407fa824d154e80c5deee78ef98067f
SelimChehwane <selim_chehwane@hotmail.com>
2024-08-01 02:53:32 +0300

    Stylin ProfileCard

--- code-editor/src/components/ProfileCard/index.jsx
+++ code-editor/src/components/ProfileCard/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import "./index.css"
 
 const ProfileCard = ({ name, email, bio, codesCount, onClick }) => {
   return (
+    <div className="window">
     <div className="profile-card" onClick={onClick}>
       <h2>{name}</h2>
       <p>Email: {email}</p>
       <p>Bio: {bio}</p>
       <p>Codes Count: {codesCount}</p>
-    </div>
+    </div></div>
   );
 };
 
